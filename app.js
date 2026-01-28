7c17f5e426e09719bd0633586a1e067a24f04b0b
MUHAMMADSufiyanDEV <sufiyangaming911@gmail.com>
2026-01-28 10:44:46 +0500

    updated

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 let rightsidebar = document.querySelector("#right-sidebar")
 
 
@@ -112,65 +112,6 @@ document.addEventListener("click",(e) => {
 
 
          
-=======
-function getingdata(params) {
-    let searchintent = document.querySelector("#search2")
-    fetching(searchintent.value)
-    }
-
-
- async function fetching(usersearch) {
-        let apiurl = await fetch(`https://forkify-api.jonas.io/api/v2/recipes?search=${usersearch}`)
-        let data = await apiurl.json()  
-        htmlui(data)
-        console.log(data)
-
- }
-
-
-        const htmlui = (data) => {
-
-            const {data:{recipes},} = data;
-
-            let HTMLUI = recipes.map( (recipie) => {
-
-                return `<div id="food-card" data-recipe-id=${recipie.id}>
-                <img src=${recipie.image_url} alt="food image">
-                <div id="food-details">
-                    <h2>${recipie.title}</h2>
-                    <p>${recipie.publisher}</p>
-                </div>
-                </div>`
-
-            })
-
-               document.querySelector("#left-sidebar").innerHTML = HTMLUI.join("")
-
-          }
-
-
-          const particularItemDetails = async (elem) => {
-
-            console.log(elem);
-            
-           
-            
-
-          }
-
-
-
-          document.addEventListener("click",(e) => {
-            if (e.target.classlist.particularItemDetails("food-card")) {
-                particularItemDetails(e.target)
-            }
-
-          })
-
-
-        
-
-
-         
->>>>>>> 317399754f72bcf41ff94419acff9d61a9f34d29
-          
+// <<<<<<< HEAD --- IGNORE ---
+// =======
+// >>>>>>> main
